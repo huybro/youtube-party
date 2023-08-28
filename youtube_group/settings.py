ea1752a489f72bb4beb8dcb97c080a5d9250bd88
Huy Cao <caogiahuy615@gmail.com>
2023-08-28 21:39:39 +0700

    updated

--- youtube_group/settings.py
+++ youtube_group/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*)rjx&(szzgtn)i&64i(5_iavy45@!kf0(1-b4*om6*qch)qpc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
